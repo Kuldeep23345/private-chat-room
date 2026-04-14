dc49911b7e6c3ddfccdd470ff0cffa7ddd440e40
Kuldeep <kunalrajput67718@gmail.com>
2026-04-14 14:25:45 +0530

    feat: implement realtime chat infrastructure with Upstash, Zod validation, and auth middleware

--- app/api/[[...slugs]]/route.ts
+++ app/api/[[...slugs]]/route.ts
@@ -1,6 +1,8 @@
 import { redis } from "@/lib/redis";
 import { Elysia } from "elysia";
 import { nanoid } from "nanoid";
+import { authMiddleware } from "./auth";
+import z from "zod";
 
 const rooms = new Elysia({ prefix: "/room" }).post("/create", async () => {
   const roomId = nanoid();
@@ -9,8 +11,24 @@ const rooms = new Elysia({ prefix: "/room" }).post("/create", async () => {
     createdAt: Date.now(),
   });
   await redis.expire(`meta:${roomId}`, 60 * 10);
-  return {roomId};
+  return { roomId };
 });
+
+const messsage = new Elysia({ prefix: "/message" }).use(authMiddleware).post(
+  "/",
+  async ({ body, auth }) => {
+    const { sender, text } = body;
+    const roomExists = await redis.exists(`meta:${auth.roomId}`);
+    if (!roomExists) {
+      throw new Error("Room does not exist");
+    }
+  },
+
+  {
+    query: z.object({ roomId: z.string() }),
+    body: z.object({ sender: z.string().max(100), text: z.string().max(1000) }),
+  },
+);
 
 export const app = new Elysia({ prefix: "/api" })
   .get("/", { user: { name: "John Doe", age: 30 } })
